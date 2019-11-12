3f41da553d899d49ccab52a3b8ebbf059beb44bb
Kadri Põldmaa <kadripoldmaa@iMac.lan>
2019-11-12 02:33:48 +0200

    add font

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigator from './navigation/Navigator';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import packsReducer from './store/packs';
+import  * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+const fetchFonts = () => {
+  Font.loadAsync({
+    'avenir-heavy': require('./assets/fonts/avenir-heavy.ttf')
+  })
+}
 
 const rootReducer = combineReducers({
   packs: packsReducer
@@ -12,11 +20,20 @@ const rootReducer = combineReducers({
 const store = createStore(rootReducer);
 
 export default function App() {
+
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return <AppLoading startAsync={fetchFonts} onFinish={() => setFontLoaded(true)} />
+  }
+
   return (
-    <Provider store={store}><Navigator /></Provider>
+    <Provider store={store}><Navigator style={styles.app} /></Provider>
   );
 }
 
 const styles = StyleSheet.create({
-
+  app:{
+    fontFamily: 'avenir-heavy',
+  }
 });
